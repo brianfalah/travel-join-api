e0e6bd9cb815401c0b2507dd911b4a70fbbdc7ad
hernan.pieroni@gmail.com <hernan.pieroni@gmail.com@e2d165d7-263c-7ff9-5d0f-3c3157898588>
2014-08-28 18:44:13 +0000

    Migracion desde xp-dev

---
+++ trunk/db/migrate/20140726210640_create_categories.rb
@@ -0,0 +1,37 @@
+#encoding: utf-8
+class CreateCategories < ActiveRecord::Migration
+  def up
+  	create_table :categories do |t|
+  		t.integer :category_id
+      t.string :name
+
+      t.timestamps
+    end                 
+        
+    #categorias principales(sin categoria padre) sacadas del webservice: http://agendacultural.buenosaires.gob.ar/webservice/response/client.php?Method=GetCategoriasListFiltered&OrdenarPor=Descripcion_es&IdCategoriaPadre=&Orden=ASC&Limit=100
+    Category.create(category_id: 88, name: "Aire Libre")
+    Category.create(category_id: 91, name: "Artes Visuales")    
+    Category.create(category_id: 56, name: "Chicos")
+    Category.create(category_id: 80, name: "Cine")
+    Category.create(category_id: 84, name: "Cultos y Colectividades")
+    Category.create(category_id: 85, name: "Circo")
+    Category.create(category_id: 148, name: "Concursos")
+    Category.create(category_id: 146, name: "Cursos y Talleres")
+    Category.create(category_id: 78, name: "Danza")
+    Category.create(category_id: 91, name: "Deportes")    
+    Category.create(category_id: 90, name: "Diseño")
+    Category.create(category_id: 79, name: "Festivales")    
+    Category.create(category_id: 82, name: "Literatura")
+    Category.create(category_id: 144, name: "Museos")
+    Category.create(category_id: 76, name: "Música")
+    Category.create(category_id: 77, name: "Teatro")
+    Category.create(category_id: 89, name: "Visitas Guiadas")
+
+    add_index :categories, :category_id
+    add_index :categories, :name
+  end
+
+  def down
+  	drop_table :categories
+  end
+end
---
+++ trunk/db/schema.rb
@@ -0,0 +1,49 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20140824231557) do
+
+  create_table "categories", :force => true do |t|
+    t.integer  "category_id"
+    t.string   "name"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "categories", ["category_id"], :name => "index_categories_on_category_id"
+  add_index "categories", ["name"], :name => "index_categories_on_name"
+
+  create_table "pois", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.decimal  "latitude",    :precision => 10, :scale => 6
+    t.decimal  "longitude",   :precision => 10, :scale => 6
+    t.string   "address"
+    t.integer  "category_id"
+    t.integer  "user_id"
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
+  end
+
+  add_index "pois", ["latitude", "longitude"], :name => "index_pois_on_latitude_and_longitude"
+
+  create_table "users", :force => true do |t|
+    t.string   "facebook_id"
+    t.string   "email"
+    t.string   "name"
+    t.string   "surname"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+end
